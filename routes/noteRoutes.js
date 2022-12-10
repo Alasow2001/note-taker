5ff50c7f9354f8a60dd64c18a7e9994c41086274
Ahmed Alasow <a.alasow01@gmail.com>
2022-12-10 18:56:41 +0000

    Added code to post and delete notes

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,4 +1,43 @@
-const express = require('express').Router;
+const note = require('express').Router;
 
-const path = require('path');
+const {v4: uuidv4} = require('uuid');
 
+const {readAndAppend, readFromFile} = require('../utils/utils');
+
+note.get('/notes', (req, res) => 
+readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
+
+note.post('/notes', (req, res) => {
+    console.log(req.body);
+
+    const {title, text} = req.body;
+
+    if(req.body){
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+        }
+
+        readAndAppend(newNote, './db/db.json');
+        res.json('New note added successfully');
+    } else{
+        res.error('Note not added. Error in adding this note.')
+    }
+});
+
+note.delete('/notes/:id', (req,res) => {
+    const noteId = req.params.note_id;
+    readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+        const result = json.filter((note) => note.note_id !== noteId);
+
+        writingFile('./db/db.json', result);
+
+        res.json(`Item ${noteId} has been removed`);
+    })
+});
+
+module.exports = note;
