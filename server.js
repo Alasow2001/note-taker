a5b7e87cfd82779eb120f6ec54aa0285941483a1
Ahmed Alasow <a.alasow01@gmail.com>
2022-12-10 21:18:09 +0000

    Updated file to go to the note page

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ app.use(express.urlencoded({extended : true}));
 app.use(express.static('public'));
 app.use('/api', apiRoute);
 
-app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
 app.get('/notes', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.listen(PORT, () => 
