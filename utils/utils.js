4f6a904213ff16593e30508d7434333d5d41ec81
Ahmed Alasow <a.alasow01@gmail.com>
2022-12-10 14:42:26 +0000

    Created and updated file that contains code to read, write and append to a file

---
+++ utils/utils.js
@@ -0,0 +1,25 @@
+// Allow us to read, write and delete files
+const fs = require('fs');
+
+// Gives access to the utility library
+const util = require('util');
+
+const read = util.promisify(fs.readFile);
+
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if(err) {
+            console.log(err);
+        }else {
+            const parseData = JSON.parse(data);
+            parseData.push(content);
+            fs.writeFile(file, parseData);
+        }
+    })
+}
+
+const write = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+module.exports = {read, write, readAndAppend};
